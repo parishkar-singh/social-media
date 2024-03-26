3083bf7cc883a1aa589685f782ebfbdb31a51a7b
Parishkar Singh <parishkar.work@gmail.com>
2024-03-26 16:59:51 +0530

    Feat: Folder Structure and Component Breakdown

---
+++ client/src/Components/ThemeProvider.tsx
@@ -0,0 +1,27 @@
+'use client'
+import {useEffect, useState} from "react";
+import {MoonStar, Sun} from "lucide-react";
+
+const ThemeToggle = () => {
+    const [theme, setTheme] = useState('light');
+
+    useEffect(() => {
+        if (theme === 'dark') {
+            document.documentElement.classList.add('dark')
+        } else {
+            document.documentElement.classList.remove('dark')
+        }
+    }, [theme])
+    const handleThemeSwitch = (e: any) => {
+        e.stopPropagation();
+        e.preventDefault();
+        setTheme(theme === 'dark' ? 'light' : 'dark')
+    }
+    return (
+        <button className={`transition bg-neutral-900 dark:bg-gray-100 duration-300 font-bold py-2 px-4 rounded-2xl`} onClick={handleThemeSwitch} >
+            {theme!=='dark'?<MoonStar className={`text-[#5a54e0]`} />:<Sun className={`text-yellow-500 `} />}
+        </button>
+
+    )
+}
+export default ThemeToggle;
