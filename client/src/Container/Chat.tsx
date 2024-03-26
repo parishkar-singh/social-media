6d4b3864f92ccd09654fdf01997fe113ec92c4c7
Parishkar Singh <parishkar.work@gmail.com>
2024-03-26 16:41:03 +0530

    feat: masterbranch cleaned

--- client/src/Container/Chat.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Chat:React.FC =()=>{
-    return (
-        <div>
-
-        </div>
-    )
-}
--- client/src/Container/Panel.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Panel:React.FC =()=>{
-    return (
-        <div>
-
-        </div>
-    )
-}
