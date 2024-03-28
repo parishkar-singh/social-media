9de7159599aace174572fa8743f520213247b903
Parishkar Singh <parishkar.work@gmail.com>
2024-03-28 18:07:45 +0530

    Refactor: Turned off the command line runner in the api applications

--- api/src/main/java/org/api/api/ApiApplication.java
+++ api/src/main/java/org/api/api/ApiApplication.java
@@ -15,31 +15,31 @@ public class ApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
     }
-    @Bean
-    CommandLineRunner run(UserService userService){
-        return args -> {
-//          userService.createRole(new Role(null,"ROLE_USER"));
-//          userService.createRole(new Role(null,"ROLE_ADMIN"));
-//          userService.createRole(new Role(null,"ROLE_OWNER"));
-        //   userService.createUser(new User(null,"parishkar singh","parishkar","parishkar.singh@esko.com","parishkar@@",new ArrayList<>()));
-//          userService.addRoleToUser("parishkar","ROLE_ADMIN");
-
-
-            // userService.createUser(new User(null, "profile-pic", "Chinmay", "chin", "chinmay.bapat@esko.com", "chinmay@@", null, null, null));
-            // userService.createUser(new User(null,  "profile-pic","Pallavi", "pall", "pallavi.bapat@esko.com", "pall@@", null, null, null));
-            
-            // userService.createUser(new User(null, "profile-pic", "indranil", "indra", "indra.bapat@esko.com", "indra@@", null, null, null));
-            // userService.createUser(new User(null, "profile-pic", "suresh", "sure", "suresh.bapat@esko.com", "suresh@@", null, null, null));
-            
-            // User temp = userService.getUserById("660534c53725550e6253b47e");
-            // temp.setUsername("SURESHBAPAT");
-            // userService.updateUser("660534c53725550e6253b47e", temp);
-
-
-
-
-
-        };
-    }
+//    @Bean
+//    CommandLineRunner run(UserService userService){
+//        return args -> {
+////          userService.createRole(new Role(null,"ROLE_USER"));
+////          userService.createRole(new Role(null,"ROLE_ADMIN"));
+////          userService.createRole(new Role(null,"ROLE_OWNER"));
+//        //   userService.createUser(new User(null,"parishkar singh","parishkar","parishkar.singh@esko.com","parishkar@@",new ArrayList<>()));
+////          userService.addRoleToUser("parishkar","ROLE_ADMIN");
+//
+//
+//            // userService.createUser(new User(null, "profile-pic", "Chinmay", "chin", "chinmay.bapat@esko.com", "chinmay@@", null, null, null));
+//            // userService.createUser(new User(null,  "profile-pic","Pallavi", "pall", "pallavi.bapat@esko.com", "pall@@", null, null, null));
+//
+//            // userService.createUser(new User(null, "profile-pic", "indranil", "indra", "indra.bapat@esko.com", "indra@@", null, null, null));
+//            // userService.createUser(new User(null, "profile-pic", "suresh", "sure", "suresh.bapat@esko.com", "suresh@@", null, null, null));
+//
+//            // User temp = userService.getUserById("660534c53725550e6253b47e");
+//            // temp.setUsername("SURESHBAPAT");
+//            // userService.updateUser("660534c53725550e6253b47e", temp);
+//
+//
+//
+//
+//
+//        };
+//    }
 
 }
